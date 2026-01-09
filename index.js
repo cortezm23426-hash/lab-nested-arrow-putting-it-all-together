d908249b12ebfbf6459ed95fa7732b85b9d9cdcf
Michael Cortez <cortezm23426@hotmail.com>
2026-01-09 11:49:01 -0500

    Complete nested arrow functions lab

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
+function createLoginTracker(userInfo) {
+  let attemptCount = 0;
+
+  return (passwordAttempt) => {
+    // If they've already made 3 attempts, lock the account
+    if (attemptCount >= 3) {
+      return "Account locked due to too many failed login attempts";
+    }
+
+    // This attempt counts (1, 2, 3)
+    attemptCount += 1;
+
+    // If correct password on attempt 1-3
+    if (passwordAttempt === userInfo.password) {
+      return "Login successful";
+    }
+
+    // Wrong password on attempt 1-3
+    return `Attempt ${attemptCount}: Login failed`;
+  };
+}
+
+// Export both ways to satisfy any import style
+module.exports = createLoginTracker;
+module.exports.createLoginTracker = createLoginTracker;
 
 
 
-module.exports = {
-  ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
-};
+
